c992656c560b4ef4ede2e64cc72043f03839bd88
Hani-fa <hfarjamy@outlook.com>
2021-01-19 14:51:37 +0100

    Age Ify JS file has been created

---
+++ javascript/javascript1/week1/ageIfy.js
@@ -0,0 +1,15 @@
+/*
+ * Hack Your Future, Javascript, homework Week1
+ * this file includes:
+  1. Age-ify (A future age calculator)
+*/
+
+
+// 1. Age-ify (A future age calculator)
+
+const yearOfBirth = 1986;
+const yearFuture = 2027;
+
+const age = yearFuture - yearOfBirth;
+
+console.log("You will be " + age + " years old in " + yearFuture + ".");
