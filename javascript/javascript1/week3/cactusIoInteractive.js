d9cf58518eaf1eff0d0fcdaf5dbb214ec4e04ae5
Hani-fa <hfarjamy@outlook.com>
2021-02-03 00:16:37 +0100

    Today activity calculation has been added

--- javascript/javascript1/week3/cactusIoInteractive.js
+++ javascript/javascript1/week3/cactusIoInteractive.js
@@ -14,26 +14,36 @@ function addActivity(date, activity, duration) {
 };
 
 addActivity(today, "Youtube", 30);
-addActivity(today, "Instagram", 60);
+addActivity("29/1-21", "Instagram", 60);
 addActivity(today, "Playing games", 50);
+//addActivity(today, "Facebook", 40); // I added this line to check the time limit reach for today
+
 console.log(activities);
 
 // Show my status
 
-function showStatus(activityArray, limitTime = 150) {
+function showStatus(activityArray, limitTime = 100) {
     let totalDuration = 0;
+    let todayDuration = 0;
+    let todayActivityNumbers = 0;
     for (let i = 0; i < activityArray.length; i++) {
         totalDuration += activityArray[i].duration;
+        if (activityArray[i].date === today) {
+            todayDuration += activityArray[i].duration;
+            todayActivityNumbers++
+        }
     }
+
     if (!activityArray || !activityArray.length) {
-        return "Add some activities before calling showStatus";
-    } else if (totalDuration > limitTime) {
-        return "You have reached your limit, no more smartphoning for you!"
+        console.log("Add some activities before calling showStatus");
+    } else if (todayDuration > limitTime) {
+        console.log("You have reached your limit, no more smartphoning for you!");
     } else {
-        return `You have added ${activityArray.length} activities. They amount to ${totalDuration} min. of usage`;
+        console.log(`You have added ${todayActivityNumbers} activities today. They amount to ${todayDuration} min. of usage`);
+        console.log(`You have added ${activityArray.length} activities in total. They amount to ${totalDuration} min. of usage`);
     }
 };
-console.log(showStatus(activities));
+showStatus(activities);
 
 function getMaximumDurationActivity() {
     let maxDuration = 0;
