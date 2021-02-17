998c90ebb49566a770540267011cc541d2ca3aa7
Hani-fa <hfarjamy@outlook.com>
2021-02-17 10:27:31 +0100

    hyfBay files have been implemented

---
+++ javascript/javascript2/week1/hyfBay/main.js
@@ -0,0 +1,32 @@
+console.log("Script loaded");
+
+const products = getAvailableProducts();
+console.log(products);
+
+function renderProducts(products) {
+    // your code here
+    let productsUl = document.createElement("ul");
+
+    products.forEach(product => {
+        let productLi = document.createElement("li");
+
+        let header = document.createElement("h1");
+        let productPrice = document.createElement("p");
+        let productRating = document.createElement("p");
+
+        header.innerHTML = product.name;
+        productPrice.innerHTML = `Price : ${product.price}`;
+        productRating.innerHTML = `Rating : ${product.rating}`;
+
+        productLi.appendChild(header);
+        productLi.appendChild(productPrice);
+        productLi.appendChild(productRating);
+
+        productsUl.appendChild(productLi);
+    });
+
+    let Body = document.querySelector("body");
+    Body.appendChild(productsUl);
+}
+
+renderProducts(products); // This should create the ul and the li's with the individual products details
