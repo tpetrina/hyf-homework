406d301d3cb4fc5fb9c1d3e40b71631878c09193
Hani-fa <hfarjamy@outlook.com>
2021-04-26 21:50:46 +0200

    circle calculator file has been created

---
+++ nodejs/week1/Circle calculator/circle_calculator.js
@@ -0,0 +1,37 @@
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    getDiameter() {
+        return this.radius * 2;
+    };
+
+    getCircumference() {
+        return this.radius * 2 * Math.PI;
+    };
+
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    };
+}
+
+const circleOne = new Circle(10);
+console.log(`circleOne diameter is ${circleOne.getDiameter()}`);
+console.log(`circleOne circumference is ${circleOne.getCircumference()}`);
+console.log(`circleOne area is ${circleOne.getArea()}`);
+
+const circleTwo = new Circle(7);
+console.log(`circleTwo diameter is ${circleTwo.getDiameter()}`);
+console.log(`circleTwo circumference is ${circleTwo.getCircumference()}`);
+console.log(`circleTwo area is ${circleTwo.getArea()}`);
+
+// Results from node
+// hfarj@iMac-KQ2J6JF MINGW64 ~/Desktop/hyf-homework/nodejs/week1/Circle calculator (nodejs/week1)
+// $ node ./circle_calculator
+// circleOne diameter is 20
+// circleOne circumference is 62.83185307179586
+// circleOne area is 314.1592653589793
+// circleTwo diameter is 14
+// circleTwo circumference is 43.982297150257104
+// circleTwo area is 153.93804002589985
